f120ce86b696dfdaa766334c2066ba2102f58ffa
Jehyeok <hook3748@gmail.com>
2014-07-31 16:45:05 +0900

    2번째

--- todolist.js
+++ todolist.js
@@ -19,8 +19,30 @@ function insertTODO(e) {
 		var todo = getTodo(this.value);
 
 		todoList.insertAdjacentHTML('beforeend', todo);
+		todoList.lastElementChild.className = 'appended';
+
 		this.value = '';
+
+		// 리무브 이벤트 등록
+		addRemoveEventToNewElement(todoList);
 	}
+}
+
+function addRemoveEventToNewElement() {
+	var todo = todoList.lastElementChild;
+	
+	// 리무브 이벤트 등록
+	todo.querySelector('.destroy').addEventListener('click', removeTODO.bind(this, todoList), false);
+}
+
+function removeTODO(todoList, e) {
+	// todo 제거
+	var todo = e.target.parentNode.parentNode;
+	todo.className = 'deleted';
+
+	todo.addEventListener('transitionend', function(e) {
+		if (e.target.tagName === 'LI') todoList.removeChild(todo);
+	}, false);
 }
 
 todoInput.addEventListener('keydown', insertTODO, false);
